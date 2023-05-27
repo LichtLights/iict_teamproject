02c04be961fc4fecba4ac13a1a82bde149f56ef8
LichtLights <94329401+LichtLights@users.noreply.github.com>
2023-05-27 20:35:52 +0900

    230527

--- sketch.js
+++ sketch.js
@@ -3,8 +3,10 @@ var trackingData;
 
 var gameState = 'title';
 
-var selectedColors;
-var isColorOn = false;
+var selectedColor;
+var isColorTrackOn = false;
+
+var score = 0;
 
 const perfectThreshold = 5;
 const goodThreshold = 10;
@@ -26,11 +28,11 @@ function stateSelector() {
             break;
 
         case 'tutorial_2':
-            tutorial_1();
+            tutorial_2();
             break;
 
         case 'tutorial_3':
-            tutorial_1();
+            tutorial_3();
             break;
 
         case 'gameIngame':
@@ -92,7 +94,7 @@ function tutorial_1() {
             2 * width / 3, height);
 
         if (mouseX <= width / 3) {
-            fill('red');
+            fill('magenta');
             quad(0, 0,
                 width / 3, 0,
 
@@ -100,7 +102,7 @@ function tutorial_1() {
                 0, height);
         }
         else if (mouseX >= width / 3 && mouseX <= 2 * width / 3) {
-            fill('green');
+            fill('cyan');
             quad(width / 3, 0,
                 2 * width / 3, 0,
 
@@ -108,7 +110,7 @@ function tutorial_1() {
                 width / 3, height);
         }
         else if (mouseX >= 2 * width / 3) {
-            fill('blue');
+            fill('yellow');
             quad(2 * width / 3, 0,
                 width, 0,
 
@@ -116,19 +118,45 @@ function tutorial_1() {
                 2 * width / 3, height);
         }
 
+        const colorInstructions = '마법봉의 색을 골라주세요!';
+        textAlign(CENTER, CENTER);
+        textSize(24);
+        fill(255);
+        text(colorInstructions, width / 2, height / 2 - 50);
+
     }
 }
 
 function tutorial_2() {
     if (gameState === 'tutorial_2') {
         background(0);
-        
+
+        const moveInstructions = '카메라를 향해 마법봉을 움직여보세요!';
+        textAlign(CENTER, CENTER);
+        textSize(24);
+        fill(255);
+        text(moveInstructions, width / 2, height / 2 - 50);
+
+
+
+        rectMode(CENTER);
+        fill(255);
+        rect(width / 2, 3 * height / 4, width / 4, height / 6);
+
+        if (mouseX >= width / 2 - width / 4 / 2 && mouseX <= width / 2 + width / 4 / 2 && mouseY >= 3 * height / 4 - height / 6 && mouseY <= 3 * height / 4 + height / 6) {
+            rectMode(CENTER);
+            fill(120);
+            rect(width / 2, 3 * height / 4, width / 4, height / 6);
+        }
     }
 }
 
 function tutorial_3() {
     if (gameState === 'tutorial_3') {
         background(0);
+
+
+        
 
     }
 }
@@ -166,19 +194,22 @@ function mouseClicked() {
 
         case 'tutorial_1':
             if (mouseX <= width / 3) {
-                selectedColors = 'magenta';
+                selectedColor = 'magenta';
             }
             else if (mouseX >= width / 3 && mouseX <= 2 * width / 3) {
-                selectedColors = 'green';
+                selectedColor = 'cyan';
             }
             else if (mouseX >= 2 * width / 3) {
-                selectedColors = 'blue';
+                selectedColor = 'yellow';
             }
-            isColorOn = true;
             gameState = 'tutorial_2';
+            trackingStart();
             break;
 
         case 'tutorial_2':
+            if (mouseX >= width / 2 - width / 4 / 2 && mouseX <= width / 2 + width / 4 / 2 && mouseY >= 3 * height / 4 - height / 6 && mouseY <= 3 * height / 4 + height / 6) {
+                gameState = 'tutorial_3';
+            }
             break;
 
         case 'tutorial_3':
@@ -210,6 +241,21 @@ function setup() {
     capture.id("myVideo"); //give the capture an ID so we can use it in the tracker below.
 }
 
+function trackingStart() {
+    if (isColorTrackOn == false) {
+        // colors = new tracking.ColorTracker(['magenta', 'cyan', 'yellow']);
+        trackingColor = new tracking.ColorTracker(selectedColor);
+
+        tracking.track('#myVideo', trackingColor); // start the tracking of the colors above on the camera in p5
+
+        //start detecting the tracking
+        trackingColor.on('track', function (event) { //this happens each time the tracking happens
+            trackingData = event.data // break the trackingjs data into a global so we can access it with p5
+        });
+        isColorTrackOn = true;
+    }
+}
+
 function draw() {
 
     stateSelector();
@@ -217,22 +263,8 @@ function draw() {
 
 }
 
-function trackingStart() {
-    if (isColorOn === true) {
-        // colors = new tracking.ColorTracker(['magenta', 'cyan', 'yellow']);
-        selectedColors = new tracking.ColorTracker(['magenta']);
-
-        tracking.track('#myVideo', selectedColors); // start the tracking of the colors above on the camera in p5
-
-        //start detecting the tracking
-        selectedColors.on('track', function (event) { //this happens each time the tracking happens
-            trackingData = event.data // break the trackingjs data into a global so we can access it with p5
-        });
-    }
-}
-
 function drawTrackingEffect() {
-    if (gameState != 'title' && gameState != 'tutorial_1') {
+    if (isColorTrackOn === true) {
         if (trackingData) { //if there is tracking data to look at, then...
             for (var i = 0; i < trackingData.length; i++) { //loop through each of the detected colors
                 // console.log( trackingData[i] )
@@ -241,3 +273,41 @@ function drawTrackingEffect() {
         }
     }
 }
+
+function playerStroke() {
+
+    
+}
+
+class RhythmNote {
+    constructor(x, y, size, color, timing) {
+      this.x = x;
+      this.y = y;
+      this.size = size;
+      this.color = color;
+      this.timing = timing;
+    }
+  
+    display() {
+      fill(this.color);
+      ellipse(this.x, this.y, this.size);
+    }
+  
+    checkTiming(playerTiming) {
+      // Compare the player's timing with the rhythm note's timing
+      // You can use the perfectThreshold and goodThreshold constants to define the timing windows
+      if (abs(playerTiming - this.timing) <= perfectThreshold) {
+        // Player hit the note perfectly
+        score += 100;
+        return 'perfect';
+      } else if (abs(playerTiming - this.timing) <= goodThreshold) {
+        // Player hit the note within the good threshold
+        score += 60;
+        return 'good';
+      } else {
+        // Player missed the note
+        return 'miss';
+      }
+    }
+  }
+  
